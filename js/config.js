ca06fc5056bfe7315d25c1f3291aca8556062d86
lizafestas <lizadecorpegueemonte@gmail.com>
2026-07-30 10:53:27 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@
    Configurações globais: Supabase, constantes
    ===================================================== */
 
-var SUPA_URL = 'https://akzsafpxfuzfbzwroejz.supabase.co';
-var SUPA_KEY = 'sb_publishable_0277ydzjBzKJJ29M2xACDA_AoUxzzoh';
+var SUPA_URL = 'https://fbgocszoesdavtdrjmuo.supabase.co';
+var SUPA_KEY = 'sb_publishable_hI1Inc85BF9nggP2rAtpCw_D_S39XrI';
 
 var _sincronizando = false;
 var _inicializando = true; // Bloqueia sync com Supabase durante boot
